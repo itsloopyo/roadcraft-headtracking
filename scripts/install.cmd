13686526a7b26b75ddfcbada164d142cc837b39d
Loop <252084443+itsloopyo@users.noreply.github.com>
2026-09-16 17:54:13 +0100

    Release v0.1.0

--- scripts/install.cmd
+++ scripts/install.cmd
@@ -22,7 +22,7 @@ set "GAME_ID=roadcraft"
 set "MOD_DISPLAY_NAME=RoadCraft Head Tracking"
 set "MOD_DLLS=RoadCraftHeadTracking.asi"
 set "MOD_INTERNAL_NAME=RoadCraftHeadTracking"
-set "MOD_VERSION=0.0.0"
+set "MOD_VERSION=0.1.0"
 set "STATE_FILE=.headtracking-state.json"
 set "FRAMEWORK_TYPE=ASILoader"
 :: Filename the ASI loader DLL is renamed to: the import the game exe already
